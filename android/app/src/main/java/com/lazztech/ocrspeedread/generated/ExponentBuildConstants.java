4f0de40fc172bab6f59fb3407251e0965a11e00b
Gian Lazzarini <gianlazzarini@gmail.com>
2017-09-07 19:43:18 -0700

    Part of the build process

--- android/app/src/main/java/com/lazztech/ocrspeedread/generated/ExponentBuildConstants.java
+++ android/app/src/main/java/com/lazztech/ocrspeedread/generated/ExponentBuildConstants.java
@@ -3,6 +3,6 @@ package com.lazztech.ocrspeedread.generated;
 // This file is auto-generated. Please don't rename!
 public class ExponentBuildConstants {
 
-  public static final String DEVELOPMENT_URL = "exp507d8d28022d4526b42cc91fa8ffe8d9://na-rxm.gianlazzarini.react-native-ocr-speed-read.exp.direct:80";
+  public static final String DEVELOPMENT_URL = "exp507d8d28022d4526b42cc91fa8ffe8d9://9a-73r.gianlazzarini.react-native-ocr-speed-read.exp.direct:80";
 
 }
