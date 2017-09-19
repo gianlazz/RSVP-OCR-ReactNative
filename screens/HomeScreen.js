69a6b476211ecb41aa4a4923ff4a5c7679c12425
Gian Lazzarini <gianlazzarini@gmail.com>
2017-09-19 15:11:07 -0700

    Deleted textDetection() leaving just documentDetection()

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -96,46 +96,6 @@ takePicture = async function() {
       }
     };
 
-  textDetextion() {
-    this.camera.capture()
-      .then((image64) => {
-        console.log(image64.data);
-//This right here is the part doing the cloud vision api calls
-        axios.post(cloudVision, {
-          requests: [
-            {
-              image: {
-                content: image64.data
-              },
-              features: [{
-                //Or 'DOCUMENT_TEXT_DETECTION'
-                type: 'TEXT_DETECTION',
-                maxResults: 1
-              }]
-            }
-          ]
-        })
-        .then(function (response) {
-        console.log(response);
-        //var json = JSON.parse(response);
-        //console.log('json parse results: ' + json.data.responses.textAnnotations.description);
-
-        //Here he's setting a const variable to hold the different json object results
-        //from the cloud vision api.
-          const textAnnotations = response.data.responses[0].textAnnotations[0];
-          const textContent = textAnnotations.description;
-
-          Alert.alert(
-           'Google Cloud Vision',
-           'Text Results: ' + textContent);
-
-        })
-        .catch(function (error) {
-          console.log(error, 'error');
-        });
-      }).catch(err => console.error(err));
-  }
-
   documentDetection() {
     this.camera.capture()
       .then((image64) => {
