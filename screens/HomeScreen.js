2ae67e873f83bdec6f5df25a9dfb6effb57c10b1
Gian Lazzarini <gianlazzarini@gmail.com>
2017-09-23 22:47:45 -0700

    Minor syntax change

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -64,7 +64,7 @@ export default class HomeScreen extends React.Component {
   }
 
 takePicture = async function() {
-this.refs.modal1.open()  
+this.refs.modal1.open()
   if (this.camera) {
     this.camera.takePictureAsync({
       quality: 0.1,
