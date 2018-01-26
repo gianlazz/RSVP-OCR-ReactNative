5fc62599f79bb998da5ac77ce7dc07dfb7c9c004
Gian Lazzarini <gianlazzarini@gmail.com>
2018-01-25 18:42:15 -0800

    Added changes to the core

--- RSVP-OCR.Core/RsvpOcrCore.ts
+++ RSVP-OCR.Core/RsvpOcrCore.ts
@@ -4,8 +4,19 @@
 // Gian Lazzarini
 // 01.22.18
 
+import axios from 'axios';
+//import m = require("axios");
+
+// API KEYS
+const cloudVisionKey = 'AIzaSyADh2eyiZCxc4g1IMjc0PjQFudxKlFW3-s';
+// Endpoints
+const cloudVision = 'https://vision.googleapis.com/v1/images:annotate?key=' + cloudVisionKey;
+var textContent:string;
+var ocrArray:Array<String>;
+var ocrString:string;
+
+
 class RsvpOcrCore {
-    //properties if needed
 
     constructor()
     {
@@ -17,15 +28,40 @@ class RsvpOcrCore {
         console.log("hello world");
     }
 
-    static ParseString(textToParse: string) 
+    static OCR(data:string, base64:boolean )
     {
-        var SplitString = textToParse.split(" ");
-        for (let index = 0; index < SplitString.length; index++) {
-            const element = textToParse[index];
-            console.log(element);            
+        axios.post(cloudVision, {
+            requests: 
+            [{
+                image: {
+                content: data
+                },
+                features: [{
+                //Or 'TEXT_DETECTION'
+                type: 'DOCUMENT_TEXT_DETECTION',
+                maxResults: 1
+                }]
+            }]
+        }).then(function (response) {
+        console.log(response);
+            var textAnnotations = response.data.responses[0].textAnnotations[0];
+            var textContent = textAnnotations.description;
+            textContent
+            console.log(textContent);
+            ocrArray = response.data.responses[0].textAnnotations.slice(1).map(w => w.description)})
+            ocrString = textContent;
+                }.bind(this)).catch(function (error) {
+                    console.log(error, 'error');
+                }).catch(err => console.error(err));
+                
+        static RsvpParseString(textToParse: string) 
+        {
+            var SplitString = textToParse.split(" ");
+            for (let index = 0; index < SplitString.length; index++) {
+                const element = textToParse[index];
+                console.log(element);            
+            }
         }
+
     }
-
-}
-
-RsvpOcrCore.ParseString("Hi This Is My First Test")
+RsvpOcrCore.RsvpParseString("Hi This Is My First Test");
