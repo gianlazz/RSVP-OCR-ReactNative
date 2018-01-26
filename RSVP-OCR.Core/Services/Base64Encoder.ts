5a1601a9512ace1e6b5084eb8f502a1645b42972
Gian Lazzarini <gianlazzarini@Gians-MacBook-Pro.local>
2018-01-26 13:34:42 -0800

    Added Base64 Encoding dependency notes for that service class

--- RSVP-OCR.Core/Services/Base64Encoder.ts
+++ RSVP-OCR.Core/Services/Base64Encoder.ts
@@ -1,8 +1,13 @@
 // May require external dependency to do Base64 encoding efficiently
+// https://www.npmjs.com/package/base-64
+// https://www.npmjs.com/package/js-base64 has typescript definitions
+// https://github.com/DefinitelyTyped/DefinitelyTyped
+// https://www.npmjs.com/package/base64-js
 
 class Base64Encoder
 {
-    static EncodeToBase64(): string
+    // Should it also accept urls to pull the images from?
+    static EncodeToBase64(imgDirectory:string): string
     {
         return "";
     }
