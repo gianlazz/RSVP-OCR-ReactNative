86cd4f6f5d2060688ceda5fec626478820dc264f
Gian Lazzarini <gianlazzarini@Gians-MacBook-Pro.local>
2018-01-25 21:09:13 -0800

    Stubbing out classes and API routines methods with Pseudocode Programming Process

--- RSVP-OCR.Core/RsvpOcrCore.ts
+++ RSVP-OCR.Core/RsvpOcrCore.ts
@@ -30,38 +30,50 @@ class RsvpOcrCore {
 
     static OCR(data:string, base64:boolean )
     {
-        axios.post(cloudVision, {
-            requests: 
-            [{
-                image: {
-                content: data
-                },
-                features: [{
-                //Or 'TEXT_DETECTION'
-                type: 'DOCUMENT_TEXT_DETECTION',
-                maxResults: 1
-                }]
-            }]
-        }).then(function (response) {
-        console.log(response);
-            var textAnnotations = response.data.responses[0].textAnnotations[0];
-            var textContent = textAnnotations.description;
-            textContent
-            console.log(textContent);
-            ocrArray = response.data.responses[0].textAnnotations.slice(1).map(w => w.description)})
-            ocrString = textContent;
-                }.bind(this)).catch(function (error) {
-                    console.log(error, 'error');
-                }).catch(err => console.error(err));
-                
-        static RsvpParseString(textToParse: string) 
-        {
-            var SplitString = textToParse.split(" ");
-            for (let index = 0; index < SplitString.length; index++) {
-                const element = textToParse[index];
-                console.log(element);            
-            }
-        }
-
+        // Accept image directory string or base64 encoded image string
+        // If it's an image directory 
+        //     Then encode it as base64
+        // Perform Optical Character Recognition
+        // Parse the JSON result from OCR
+        // If the resulting JSON OCR result string object contains a URL
+        //     Then see if the url is valid
+        //     Optionally verify with user that the URL is the one that they want
+        //     If the user selects a found URL
+        //         Pull the primary reading text of that page, stripped of adds or bullshit
+        // Return the final string to be visualized
+        
+        // axios.post(cloudVision, {
+        //     requests: 
+        //     [{
+        //         image: {
+        //         content: data
+        //         },
+        //         features: [{
+        //         //Or 'TEXT_DETECTION'
+        //         type: 'DOCUMENT_TEXT_DETECTION',
+        //         maxResults: 1
+        //         }]
+        //     }]
+        // }).then(function (response) {
+        // console.log(response);
+        //     var textAnnotations = response.data.responses[0].textAnnotations[0];
+        //     var textContent = textAnnotations.description;
+        //     textContent
+        //     console.log(textContent);
+        //     ocrArray = response.data.responses[0].textAnnotations.slice(1).map(w => w.description)})
+        //     ocrString = textContent;
+        //         }.bind(this)).catch(function (error) {
+        //             console.log(error, 'error');
+        //         }).catch(err => console.error(err));
     }
+
+    static RsvpParseString(textToParse: string)
+    {
+        var SplitString = textToParse.split(" ");
+        for (let index = 0; index < SplitString.length; index++) {
+            const element = textToParse[index];
+            console.log(element);            
+        }
+    }
+}
 RsvpOcrCore.RsvpParseString("Hi This Is My First Test");
--- RSVP-OCR.Core/dist/RsvpOcrCore.js
+++ RSVP-OCR.Core/dist/RsvpOcrCore.js
@@ -4,15 +4,59 @@
 // All unit tests should initially point here, while any dependencies are encapulated away
 // Gian Lazzarini
 // 01.22.18
+Object.defineProperty(exports, "__esModule", { value: true });
+//import m = require("axios");
+// API KEYS
+var cloudVisionKey = 'AIzaSyADh2eyiZCxc4g1IMjc0PjQFudxKlFW3-s';
+// Endpoints
+var cloudVision = 'https://vision.googleapis.com/v1/images:annotate?key=' + cloudVisionKey;
+var textContent;
+var ocrArray;
+var ocrString;
 var RsvpOcrCore = /** @class */ (function () {
-    //properties if needed
     function RsvpOcrCore() {
         //constructor most likely wont be needed as multiple instances of this class are not likely to be called
     }
     RsvpOcrCore.HelloWorld = function () {
         console.log("hello world");
     };
-    RsvpOcrCore.ParseString = function (textToParse) {
+    RsvpOcrCore.OCR = function (data, base64) {
+        // Accept image directory string or base64 encoded image string
+        // If it's an image directory 
+        //     Then encode it as base64
+        // Perform Optical Character Recognition
+        // Parse the JSON result from OCR
+        // If the resulting JSON OCR result string object contains a URL
+        //     Then see if the url is valid
+        //     Optionally verify with user that the URL is the one that they want
+        //     If the user selects a found URL
+        //         Pull the primary reading text of that page, stripped of adds or bullshit
+        // Return the final string to be visualized
+        // axios.post(cloudVision, {
+        //     requests: 
+        //     [{
+        //         image: {
+        //         content: data
+        //         },
+        //         features: [{
+        //         //Or 'TEXT_DETECTION'
+        //         type: 'DOCUMENT_TEXT_DETECTION',
+        //         maxResults: 1
+        //         }]
+        //     }]
+        // }).then(function (response) {
+        // console.log(response);
+        //     var textAnnotations = response.data.responses[0].textAnnotations[0];
+        //     var textContent = textAnnotations.description;
+        //     textContent
+        //     console.log(textContent);
+        //     ocrArray = response.data.responses[0].textAnnotations.slice(1).map(w => w.description)})
+        //     ocrString = textContent;
+        //         }.bind(this)).catch(function (error) {
+        //             console.log(error, 'error');
+        //         }).catch(err => console.error(err));
+    };
+    RsvpOcrCore.RsvpParseString = function (textToParse) {
         var SplitString = textToParse.split(" ");
         for (var index = 0; index < SplitString.length; index++) {
             var element = textToParse[index];
@@ -21,4 +65,4 @@ var RsvpOcrCore = /** @class */ (function () {
     };
     return RsvpOcrCore;
 }());
-RsvpOcrCore.ParseString("Hi This Is My First Test");
+RsvpOcrCore.RsvpParseString("Hi This Is My First Test");
