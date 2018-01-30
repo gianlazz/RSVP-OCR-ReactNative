25c524bf63faf7a4aca0b1892f49d1a4997b9b74
Gian Lazzarini <gianlazzarini@Gians-MacBook-Pro.local>
2018-01-29 19:34:52 -0800

    Stubbed out 4 expected unit test "Suites"

--- RSVP-OCR.Core/test/test.js
+++ RSVP-OCR.Core/test/test.js
@@ -2,16 +2,46 @@
 var expect = require('chai').expect;
 var core = require('../dist/RsvpOcrCore.js');
 
-describe('Valid Base64 encoded image', () => {
-    it('should return true', () => {
-        var result = core.Parse("hi");
-        expect(result).to.equal(3);
+describe('Local image directory', () => {
+    it('if valid should return ___', () => {
+        //var result = core.Parse("hi");
+        //expect(result).to.equal(false);
+    });
+    it('if invalid should return ___', () => {
+        //var result = core.Parse("hi");
+        //expect(result).to.equal(false);
     });
 });
 
-describe('Corrupt or invalid Base64 encoded image', () => {
-    it('should return false', () => {
-        var result = core.Parse("hi");
-        expect(result).to.equal(3);
+describe('Base64 encoded image', () => {
+    it('if valid should return ___', () => {
+        //var result = core.Parse("hi");
+        //expect(result).to.equal(false);
+    });
+    it('if invalid should return ___', () => {
+        //var result = core.Parse("hi");
+        //expect(result).to.equal(false);
+    });
+});
+
+describe('Image Url', () => {
+    it('if valid should return ___', () => {
+        //var result = core.Parse("hi");
+        //expect(result).to.equal(false);
+    });
+    it('if invalid should return ___', () => {
+        //var result = core.Parse("hi");
+        //expect(result).to.equal(false);
+    });
+});
+
+describe('Webpage Url', () => {
+    it('if valid should return ___', () => {
+        //var result = core.Parse("hi");
+        //expect(result).to.equal(false);
+    });
+    it('if invalid should return ___', () => {
+        //var result = core.Parse("hi");
+        //expect(result).to.equal(false);
     });
 });
